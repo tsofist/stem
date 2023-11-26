68339623c667d7685132b95e2692b0d822c60e54
Andrew Berdnikov <tsofistgudmen@gmail.com>
2023-11-25 18:38:42 -0600

    feat(object): entries is now supports for sort elements

---
+++ src/object/entries.spec.ts
@@ -0,0 +1,70 @@
+import { entries } from './entries';
+
+describe('entries', () => {
+    it('should return an empty array for null or undefined target', () => {
+        expect(entries(null)).toEqual([]);
+        expect(entries(undefined)).toEqual([]);
+    });
+
+    it('should return an array of key-value pairs for an object', () => {
+        const obj = { a: 1, b: 'two', c: true };
+        expect(entries(obj)).toEqual([
+            ['a', 1],
+            ['b', 'two'],
+            ['c', true],
+        ]);
+    });
+
+    it('should return an array of key-value pairs for a class instance', () => {
+        class MyClass {
+            a = 1;
+            b = 'two';
+            c = true;
+        }
+
+        const instance = new MyClass();
+        expect(entries(instance)).toEqual([
+            ['a', 1],
+            ['b', 'two'],
+            ['c', true],
+        ]);
+    });
+
+    it('should return an array of key-value pairs for a Map object', () => {
+        const map = new Map<string, number>([
+            ['a', 1],
+            ['b', 2],
+            ['c', 3],
+        ]);
+        console.log('MAP', entries(map));
+        expect(entries(map)).toEqual([
+            ['a', 1],
+            ['b', 2],
+            ['c', 3],
+        ]);
+    });
+
+    it('should return an array of key-value pairs for a Set object', () => {
+        const set = new Set<string>(['a', 'b', 'c']);
+        console.log('SET', entries(set));
+        expect(entries(set)).toEqual([
+            ['a', 'a'],
+            ['b', 'b'],
+            ['c', 'c'],
+        ]);
+    });
+
+    it('should be sorted by key if a sort function is provided', () => {
+        const obj = { b: 'two', a: 1, c: true };
+        expect(entries(obj, (a, b) => (a[0] < b[0] ? -1 : 1))).toEqual([
+            ['a', 1],
+            ['b', 'two'],
+            ['c', true],
+        ]);
+        expect(entries(obj, (a, b) => (a[0] > b[0] ? -1 : 1))).toEqual([
+            ['c', true],
+            ['b', 'two'],
+            ['a', 1],
+        ]);
+    });
+});
--- src/object/entries.ts
+++ src/object/entries.ts
@@ -1,10 +1,17 @@
-import { Nullable } from '../index';
+import { CompareResult, Nullable } from '../index';
 
 /**
  * Type-safe version of Object.entries
  * @see https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
  */
-export function entries<T, K extends keyof T>(target: Nullable<T>): [K, T[K]][] {
-    // @ts-expect-error It's OK
-    return target == null ? [] : Object.entries(target);
+export function entries<T, K extends keyof T>(
+    target: Nullable<T>,
+    sort?: (a: [K, T[K]], b: [K, T[K]]) => CompareResult,
+): [K, T[K]][] {
+    if (!target) return [];
+    const result: [any, any][] =
+        target instanceof Set || target instanceof Map
+            ? Array.from(target.entries())
+            : Object.entries(target);
+    return sort ? result.sort(sort) : result;
 }
