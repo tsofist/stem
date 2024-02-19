5f931d268ccb5a72cf363d0a76807a8060d0b7b9
Andrew Berdnikov <tsofistgudmen@gmail.com>
2024-02-19 00:02:25 -0600

    feat: indexBy - supports for Map/Set

--- src/object/index-by.spec.ts
+++ src/object/index-by.spec.ts
@@ -1,41 +1,56 @@
 import { indexBy } from './index-by';
 
 describe('indexBy', () => {
-    const list = [
+    const list: {
+        a: string;
+        b: number | string;
+        c?: number;
+    }[] = [
         { a: 'a', b: 1 },
         { a: 'c', b: 3, c: 1 },
         { a: 'c', b: 'non', c: 2 },
     ];
+    const map = new Map(list.map((item) => [Math.random(), item]));
+    const set = new Set(list);
 
     it('string keyField', () => {
-        expect(indexBy(list, 'a')).toEqual({
-            a: { a: 'a', b: 1 },
-            c: { a: 'c', b: 'non', c: 2 },
-        });
-        expect(indexBy(list, 'c')).toEqual({
-            1: { a: 'c', b: 3, c: 1 },
-            2: { a: 'c', b: 'non', c: 2 },
-        });
+        for (const target of [list, map, set]) {
+            expect(indexBy(target, 'a')).toEqual({
+                a: { a: 'a', b: 1 },
+                c: { a: 'c', b: 'non', c: 2 },
+            });
+            expect(indexBy(target, 'c')).toEqual({
+                1: { a: 'c', b: 3, c: 1 },
+                2: { a: 'c', b: 'non', c: 2 },
+            });
+        }
     });
     it('function keyField', () => {
-        expect(indexBy(list, () => 'a')).toEqual({
-            a: { a: 'c', b: 'non', c: 2 },
-        });
-        expect(indexBy(list, (item) => (typeof item.b === 'string' ? 'STR' : 'NON-STR'))).toEqual({
-            'NON-STR': { a: 'c', b: 3, c: 1 },
-            'STR': { a: 'c', b: 'non', c: 2 },
-        });
+        for (const target of [list, map, set]) {
+            expect(indexBy(target, () => 'a')).toEqual({
+                a: { a: 'c', b: 'non', c: 2 },
+            });
+            expect(
+                indexBy(target, (item) => (typeof item.b === 'string' ? 'STR' : 'NON-STR')),
+            ).toEqual({
+                'NON-STR': { a: 'c', b: 3, c: 1 },
+                'STR': { a: 'c', b: 'non', c: 2 },
+            });
+        }
     });
     it('edge cases', () => {
         // @ts-expect-error It's OK
         expect(indexBy(null, '123')).toEqual({});
         // @ts-expect-error It's OK
         expect(indexBy(undefined, '123')).toEqual({});
-        // @ts-expect-error It's OK
-        expect(indexBy(list, '123')).toEqual({});
-        // @ts-expect-error It's OK
-        expect(indexBy(list, () => null)).toEqual({});
-        // @ts-expect-error It's OK
-        expect(indexBy(list, () => undefined)).toEqual({});
+
+        for (const target of [list, map, set]) {
+            // @ts-expect-error It's OK
+            expect(indexBy(target, '123')).toEqual({});
+            // @ts-expect-error It's OK
+            expect(indexBy(target, () => null)).toEqual({});
+            // @ts-expect-error It's OK
+            expect(indexBy(target, () => undefined)).toEqual({});
+        }
     });
 });
--- src/object/index-by.ts
+++ src/object/index-by.ts
@@ -3,22 +3,24 @@ import { Nullable, ObjectKey, PRec, SimpleValuesOf, WSimpleValueKeysOf } from '.
 export type KeyValueExtractor<T, R extends ObjectKey> = (target: T) => R;
 
 export function indexBy<T, K extends WSimpleValueKeysOf<T>>(
-    target: Nullable<T[]>,
+    target: Nullable<T[] | Set<T> | Map<any, T>>,
     keyField: K,
 ): PRec<T, SimpleValuesOf<T, K>>;
 export function indexBy<T, KV extends ObjectKey, E extends KeyValueExtractor<T, KV>>(
-    target: Nullable<T[]>,
+    target: Nullable<T[] | Set<T> | Map<any, T>>,
     keyValueExtractor: E,
 ): PRec<T, ReturnType<E>>;
 export function indexBy<T, R extends ObjectKey>(
-    target: Nullable<T[]>,
+    target: Nullable<T[] | Set<T> | Map<unknown, T>>,
     keyField: KeyValueExtractor<T, R> | keyof T,
 ): PRec<T> {
+    const isMap = target instanceof Map;
     const result: PRec<T> = Object.create(null);
     const fn = typeof keyField === 'function';
 
     if (target) {
-        for (const item of target) {
+        for (let item of target) {
+            item = isMap ? (item as [unknown, T])[1] : (item as T);
             const keyValue = fn ? keyField(item) : item[keyField];
             if (keyValue != null) {
                 // @ts-expect-error It's OK
