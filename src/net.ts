335def4fca60817a2cea355f6b91cca3f6a5cfc7
Andrew Berdnikov <tsofistgudmen@gmail.com>
2024-03-04 16:00:45 -0600

    feat: add net types

---
+++ src/net.ts
@@ -0,0 +1,16 @@
+/**
+ * IP Address V4
+ * @format ipv4
+ */
+export type IPV4String = string;
+
+/**
+ * IP Address V6
+ * @format ipv6
+ */
+export type IPV6String = string;
+
+/**
+ * IP Address
+ */
+export type IPAddrString = IPV4String | IPV6String;
