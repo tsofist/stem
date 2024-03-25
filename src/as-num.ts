5d9e6053cef5fcf398c711d0f3d6237df009cdc0
Andrew Berdnikov <tsofistgudmen@gmail.com>
2024-03-25 15:21:44 -0600

    fix: inline export for asNum

--- src/as-num.ts
+++ src/as-num.ts
@@ -1,12 +1,10 @@
-export { asNum };
-
 /**
  * Converts any value to number.
  *
  * Returns `defaultValue` if value is nullable
  * Returns `nanValue` if result value is not a number
  */
-function asNum(it: any, defaultValue = 0, nanValue = 0): number {
+export function asNum(it: any, defaultValue = 0, nanValue = 0): number {
     if (it == null) return defaultValue;
     switch (typeof it) {
         case 'boolean':
