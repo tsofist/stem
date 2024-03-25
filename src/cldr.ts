dd97d042ba8a275385e341f7286c0dbd505833d3
Andrew Berdnikov <tsofistgudmen@gmail.com>
2024-03-25 15:20:56 -0600

    fix (cldr): fix patterns for ISOTimeZString and ISODateTimeZString

--- src/cldr.ts
+++ src/cldr.ts
@@ -34,9 +34,9 @@ export type ISOTimeString = string;
 
 /**
  * ISO time (without date) in UTC time-zone
- * @pattern ^(0[0-9]|1[0-9]|2[0-3])(:[0-5][0-9]){2}Z$
+ * @pattern ^(0[0-9]|1[0-9]|2[0-3])(:[0-5][0-9]){2}(\.\d{1,3})?Z$
  */
-export type ISOTimeZString = `${number}-${number}-${number}Z`;
+export type ISOTimeZString = `${number}:${number}:${number}Z`;
 
 /**
  * ISO date with time with optional time-zone
@@ -46,7 +46,7 @@ export type ISODateTimeString = string;
 
 /**
  * ISO date with time with optional time-zone
- * @pattern ^(?:19|20)[0-9]{2}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1[0-9]|2[0-9])|(?:(?!02)(?:0[1-9]|1[0-2])-(?:30))|(?:(?:0[13578]|1[02])-31))T([0-5][0-9])\:([0-5][0-9])\:([0-5][0-9])Z$
+ * @pattern ^(?:19|20)[0-9]{2}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1[0-9]|2[0-9])|(?:(?!02)(?:0[1-9]|1[0-2])-(?:30))|(?:(?:0[13578]|1[02])-31))T(0[0-9]|1[0-9]|2[0-3])(:[0-5][0-9]){2}(\.\d{1,3})?Z$
  */
 export type ISODateTimeZString = `${string}Z`;
 
