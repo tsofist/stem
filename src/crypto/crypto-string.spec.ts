5db43e46df9562106df01eeb11be2b2dd83c1929
Andrew Berdnikov <tsofistgudmen@gmail.com>
2024-11-04 21:17:34 -0300

    feat(crypto): rework CryptoString   change default algorithm to ChaCha20   add ability to specify algorithm   support auth-tag algorithms

--- src/crypto/crypto-string.spec.ts
+++ src/crypto/crypto-string.spec.ts
@@ -1,19 +1,43 @@
-import { CryptoString } from './crypto-string';
+import { enumKeyByValue, extractEnumValues } from '../enum';
+import { CipherAlgorithm, CryptoString } from './crypto-string';
 
 describe('CryptoString', () => {
     const cryptoKey = CryptoString.createCryptoKey();
-    const cryptoString = new CryptoString(cryptoKey);
-    const testValue = 'Hello, World!';
+    const testValue = '§ Hello, World! 요 😊';
 
-    it('should encrypt and decrypt a value correctly', () => {
-        const encrypted = cryptoString.encrypt(testValue);
+    describe('Encode+Decode', () => {
+        for (const algorithm of [
+            /* All     */ ...extractEnumValues(CipherAlgorithm),
+            /* Default */ undefined,
+        ]) {
+            const algorithmName = algorithm
+                ? enumKeyByValue(CipherAlgorithm, algorithm)
+                : '[ Default algorithm ]';
+
+            it(`should encrypt and decrypt a value correctly with ${algorithmName}`, () => {
+                const cryptoString = new CryptoString(cryptoKey, algorithm);
+
+                const encrypted = cryptoString.encrypt(testValue);
+                expect(encrypted.trim()).not.toStrictEqual('');
+
+                const decrypted = cryptoString.decrypt(encrypted);
+                expect(decrypted).toStrictEqual(testValue);
+            });
+        }
+    });
+
+    it('should encrypt and decrypt an empty value correctly', () => {
+        const cryptoString = new CryptoString(cryptoKey);
+        const value = '';
+
+        const encrypted = cryptoString.encrypt(value);
+        expect(encrypted.trim()).not.toStrictEqual('');
+
         const decrypted = cryptoString.decrypt(encrypted);
-        expect(decrypted).toBe(testValue);
+        expect(decrypted).toStrictEqual(value);
     });
 
     it('should throw an error if the crypto key is empty', () => {
-        expect(() => new CryptoString('')).toThrow(
-            'Crypto key must be non-empty base64 string (use openssl rand -base64 32)',
-        );
+        expect(() => new CryptoString('')).toThrow(new RegExp('^Crypto key must be non-empty'));
     });
 });
--- src/crypto/crypto-string.ts
+++ src/crypto/crypto-string.ts
@@ -1,12 +1,25 @@
 import { Buffer } from 'buffer';
 import { BinaryLike, CipherKey, createCipheriv, createDecipheriv, randomBytes } from 'crypto';
 import { raise } from '../error';
-import { Base64String } from '../index';
-
-const CIPHER_ALGORITHM = 'aes-256-cbc';
+import { Base64String, Rec } from '../index';
+import { valueIn } from '../value-in';
 
 /**
- * A class for encrypting and decrypting data using a crypto key.
+ * Supported cipher algorithms.
+ *
+ * @example
+ *   openssl list -cipher-algorithms
+ */
+export enum CipherAlgorithm {
+    ChaCha20 = 'chacha20',
+    ChaCha20Poly1305 = 'chacha20-poly1305',
+    AES256CBC = 'aes-256-cbc',
+    AES256GCM = 'aes-256-gcm',
+}
+
+/**
+ * Functionality for encrypting strings.
+ * Suitable for storing sensitive data in a database, such as passwords.
  */
 export class CryptoString {
     static createCryptoKey(): Base64String {
@@ -14,27 +27,85 @@ export class CryptoString {
     }
 
     readonly #cryptoKey: CipherKey;
+    readonly #cipherAlgorithm: CipherAlgorithm;
+    readonly #ivLen: IVSizeValue;
+    readonly #authTagLen: number;
 
     /**
      * @see CryptoString.createCryptoKey
+     *
+     * @example
+     *   const key = CryptoString.createCryptoKey();
+     *
      * @example
      *   openssl rand -base64 32
      */
-    constructor(cryptoKey: Base64String) {
-        if (!cryptoKey || cryptoKey.length < 44)
-            raise('Crypto key must be non-empty base64 string (use openssl rand -base64 32)');
+    constructor(cryptoKey: Base64String, algorithm: CipherAlgorithm = CipherAlgorithm.ChaCha20) {
+        if (!valueIn(algorithm, CipherAlgorithm, true)) {
+            raise(`Unknown cipher algorithm: ${algorithm}`);
+        }
+        if (!cryptoKey || cryptoKey.length !== 44) {
+            raise(
+                'Crypto key must be non-empty base64-encoded string of 32 bytes (44 ch.).' +
+                    'Use CryptoString.createCryptoKey() to generate a new key.' +
+                    'Or use command: openssl rand -base64 32',
+            );
+        }
         this.#cryptoKey = Buffer.from(cryptoKey, 'base64');
+        this.#cipherAlgorithm = algorithm;
+        this.#ivLen = IVSize[algorithm];
+        this.#authTagLen = ATSize[algorithm];
     }
 
-    encrypt(value: BinaryLike): string {
-        const iv = randomBytes(16);
-        const cipher = createCipheriv(CIPHER_ALGORITHM, this.#cryptoKey, iv);
-        return Buffer.concat([iv, cipher.update(value), cipher.final()]).toString('base64');
+    encrypt(value: BinaryLike): Base64String {
+        const iv = randomBytes(this.#ivLen);
+        const cipher = createCipheriv(this.#cipherAlgorithm, this.#cryptoKey, iv, {
+            // @ts-expect-error It's OK
+            authTagLength: this.#authTagLen,
+        });
+
+        const encrypted = Buffer.concat([cipher.update(value), cipher.final()]);
+        const authTag = this.#authTagLen ? cipher.getAuthTag() : Buffer.alloc(0);
+
+        return Buffer.concat([iv, authTag, encrypted]).toString('base64');
     }
 
-    decrypt(value: string): string {
+    decrypt(value: string): Base64String {
         const data = Buffer.from(value, 'base64');
-        const decipher = createDecipheriv(CIPHER_ALGORITHM, this.#cryptoKey, data.subarray(0, 16));
-        return Buffer.concat([decipher.update(data.subarray(16)), decipher.final()]).toString();
+        const iv = data.subarray(0, this.#ivLen);
+
+        const authTag = this.#authTagLen
+            ? data.subarray(this.#ivLen, this.#ivLen + this.#authTagLen)
+            : Buffer.alloc(0);
+        const encrypted = this.#authTagLen
+            ? data.subarray(this.#ivLen + this.#authTagLen)
+            : data.subarray(this.#ivLen);
+
+        const decipher = createDecipheriv(this.#cipherAlgorithm, this.#cryptoKey, iv, {
+            // @ts-expect-error It's OK
+            authTagLength: this.#authTagLen,
+        });
+
+        if (this.#authTagLen > 0) {
+            decipher.setAuthTag(authTag);
+        }
+
+        return Buffer.concat([decipher.update(encrypted), decipher.final()]).toString('utf8');
     }
 }
+
+type IVSizeValue = (typeof IVSize)[CipherAlgorithm];
+
+const IVSize = {
+    [CipherAlgorithm.ChaCha20]: 16,
+    [CipherAlgorithm.AES256CBC]: 16,
+    [CipherAlgorithm.ChaCha20Poly1305]: 12,
+    [CipherAlgorithm.AES256GCM]: 12,
+} as const satisfies Rec<number, CipherAlgorithm>;
+
+const ATSize = {
+    [CipherAlgorithm.ChaCha20]: 0,
+    [CipherAlgorithm.AES256CBC]: 0,
+    [CipherAlgorithm.ChaCha20Poly1305]: 16,
+    [CipherAlgorithm.AES256GCM]: 16,
+} as const satisfies Rec<number, CipherAlgorithm>;
