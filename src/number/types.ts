dc73578bfaac2be0996469c96dba1ead8d17f2ae
Andrew Berdnikov <tsofistgudmen@gmail.com>
2025-03-31 17:28:14 -0300

    feat: add PositiveFloat type and update integer types

--- src/number/types.ts
+++ src/number/types.ts
@@ -5,6 +5,12 @@ export type Float = number;
 
 /**
  * @asType number
+ * @exclusiveMinimum 0
+ */
+export type PositiveFloat = number;
+
+/**
+ * @asType number
  * @minimum 0
  */
 export type NonNegativeFloat = number;
@@ -21,16 +27,19 @@ export type NegativeFloat = number;
 export type Int = number;
 
 /**
+ * @asType integer
  * @minimum 1
  */
-export type PositiveInt = Int;
+export type PositiveInt = number;
 
 /**
+ * @asType integer
  * @minimum 0
  */
-export type NonNegativeInt = Int;
+export type NonNegativeInt = number;
 
 /**
+ * @asType integer
  * @maximum -1
  */
-export type NegativeInt = Int;
+export type NegativeInt = number;
