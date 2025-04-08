cb2307aecdeb7eac1d7d742f22df9adf368b2ce8
Andrew Berdnikov <tsofistgudmen@gmail.com>
2025-04-08 01:29:53 -0300

    feat(cldr): add ISODayOfMonth and ISODayOfMonthSet types

--- src/cldr/types.ts
+++ src/cldr/types.ts
@@ -23,4 +23,14 @@ export type ISOWeekdayPRec<T> = PRec<T, ISOWeekday>;
 
 export type ISOWeekdaySet = UniqueItemsArray<ISOWeekday>;
 
+export type ISODayOfMonth =
+    /* eslint-disable prettier/prettier */
+    | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
+    | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20
+    | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30
+    | 31;
+    /* eslint-enable prettier/prettier */
+
+export type ISODayOfMonthSet = UniqueItemsArray<ISODayOfMonth>;
+
 export type DateConstructorSource = string | Int | Date;
