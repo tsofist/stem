c891a6ce2c8398e98845fc3c5377e7b283afe4c6
Andrew Berdnikov <tsofistgudmen@gmail.com>
2025-11-14 12:42:43 -0300

    feat: add `isBooleanLikeString`

--- src/as-bool.ts
+++ src/as-bool.ts
@@ -2,22 +2,44 @@
  * Converting value to boolean type.
  * In case of Nully result defaultValue will be used.
  */
-export function asBool(it: any, defaultValue = false): boolean {
+export function asBool(it: unknown, defaultValue = false): boolean {
     if (it == null) return defaultValue;
+
     switch (typeof it) {
         case 'boolean':
             return it;
         case 'number':
             return !isNaN(it) && it !== 0;
-        case 'string':
-            if (it.length === 1 || (it.length === 2 && it.charAt(0) === '-')) {
-                if (!isNaN((it = parseInt(it)))) return it !== 0;
-            } else if (it.length >= 4 && it.length <= 5) {
-                if (it.toLowerCase() === 'true') return true;
-                else if (it.toLowerCase() === 'false') return false;
+        case 'string': {
+            const v = it.toLowerCase();
+            if (v in BooleanLikeStringValues) {
+                return BooleanLikeStringValues[v as BooleanLikeStringValue];
             }
             return defaultValue;
+        }
+
         default:
             return defaultValue;
     }
 }
+
+/**
+ * Checks whether the given value is a string representing a boolean-like value.
+ * @see BooleanLikeStringValue
+ * @see BooleanLikeStringValues
+ */
+export function isBooleanLikeString(it: unknown): it is BooleanLikeStringValue {
+    return typeof it === 'string' && it.toLowerCase() in BooleanLikeStringValues;
+}
+
+export type BooleanLikeStringValue = keyof typeof BooleanLikeStringValues;
+
+const BooleanLikeStringValues = {
+    '0': false,
+    '1': true,
+    '-1': true,
+    'no': false,
+    'yes': true,
+    'true': true,
+    'false': false,
+} as const;
